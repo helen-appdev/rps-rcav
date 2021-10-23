7e70a6e2a9adc8721eec3a1e7a943c0cf294cad0
helenavocado <hwang92@uchicago.edu>
2021-10-23 00:57:01 +0000

    try 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,30 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout ("wrapper.html.erb")
+  
 
   # Add your actions below this line
   # ================================
+  def homepage
+    render({ :template => "game_templates/rules.html.erb"})
+  end
+
+  def play_rock
+    #redirect_to("https://www.ecosia.org")
+    #render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    render({:template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb"})
+  end
+
+  def play_paper
+    if @comp_move == "rock" 
+      @outcome = "won"
+    elsif @comp_move == "paper" 
+      @outcome = "tied"
+    elsif @comp_move == "scissors" 
+      @outcome = "lost"
+    end   
+    
+    render({:template => "game_templates/user_paper.html.erb"})
+
+  end
 
 end
