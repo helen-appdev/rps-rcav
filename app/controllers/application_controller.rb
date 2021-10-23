6a90f8d23bd714ab19fdc6d923ee2e08dde7351a
helenavocado <hwang92@uchicago.edu>
2021-10-23 01:10:45 +0000

    draft

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,12 +9,19 @@ class ApplicationController < ActionController::Base
   end
 
   def play_rock
-    #redirect_to("https://www.ecosia.org")
-    #render({ :html => "<h1>Hello, World!</h1>".html_safe})
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock" 
+      @outcome = "tied"
+    elsif @comp_move == "paper" 
+      @outcome = "lost"
+    elsif @comp_move == "scissors" 
+      @outcome = "won"
+    end
     render({:template => "game_templates/user_rock.html.erb", :layout => "wrapper.html.erb"})
   end
 
   def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
     if @comp_move == "rock" 
       @outcome = "won"
     elsif @comp_move == "paper" 
@@ -22,9 +29,19 @@ class ApplicationController < ActionController::Base
     elsif @comp_move == "scissors" 
       @outcome = "lost"
     end   
-    
     render({:template => "game_templates/user_paper.html.erb"})
+  end
 
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock" 
+      @outcome = "lost"
+    elsif @comp_move == "paper" 
+      @outcome = "won"
+    elsif @comp_move == "scissors" 
+      @outcome = "tied"
+    end   
+    render({:template => "game_templates/user_scissors.html.erb"})
   end
 
 end
